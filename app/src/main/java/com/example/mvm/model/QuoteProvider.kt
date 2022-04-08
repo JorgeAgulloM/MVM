ace5695f8513a2e308e82c1b6e8d1cfef6ecf147
Jorge Agulló <agullojorge@gmail.com>
2022-04-08 09:24:22 +0200

    fix error in random range. Finish first version

--- app/src/main/java/com/example/mvm/model/QuoteProvider.kt
+++ app/src/main/java/com/example/mvm/model/QuoteProvider.kt
@@ -4,63 +4,63 @@ package com.example.mvm.model
 class QuoteProvider {
     companion object  {
         fun random():QuoteModel{
-            val position:Int = (0..11).random()
+            val position:Int = (0..10).random()
             return quote[position]
         }
 
         private val quote = listOf<QuoteModel>(
-            QuoteModel(
+            QuoteModel( //0
                 quote = "Los ordenadores son inútiles. Sólo pueden darte respuestas",
                 author = "Pablo Picasso"
             ),
-            QuoteModel(
+            QuoteModel( //1
                 quote = "Los ordenadores son como los bikinis. Ahorran a la gente el hacer muchas conjeturas",
                 author = "Sam Ewing"
             ),
-            QuoteModel(
+            QuoteModel( //2
                 quote = "Tienen ordenadores, y pueden tener otras armas de destrucción masiva",
                 author = "Janet Reno"
             ),
-            QuoteModel(
+            QuoteModel( //3
                 quote = "Si la industria automovilística hubiera seguido el mismo desarrollo que los " +
                         "ordenadores, un Rolls-Royce costaría hoy 100 dólares, circularía un millón de " +
                         "millas con 3,7 litros y explotaría una vez al año, eliminando a todo el que " +
                         "estuviera dentro en ese momento",
                 author = "Robert X. Cringely"
             ),
-            QuoteModel(
+            QuoteModel( //4
                 quote = "Los ordenadores se hacen cada vez más inteligentes. Los científicos dicen que " +
                         "pronto ellos serán capaces de hablarnos (y con 'ellos' me refiero a los " +
                         "ordenadores, dudo mucho que los científicos sean capaces de hablarnos)",
                 author = "Dave Barry"
             ),
-            QuoteModel(
+            QuoteModel( //5
                 quote = "He notado últimamente que el miedo paranoico hacia ordenadores inteligentes " +
                         "tomando el control del mundo ha desaparecido totalmente. Todo lo que puedo " +
                         "contar es que este hecho coincide con la aparición de MS-DOS",
                 author = "Larry DeLuca"
             ),
-            QuoteModel(
+            QuoteModel( //6
                 quote = "Preguntarse cuándo los ordenadores podrán pensar es como preguntarse cuándo " +
                         "los submarinos podrán nadar",
                 author = "Edsger W. Dijkstra"
             ),
-            QuoteModel(
+            QuoteModel( //7
                 quote = "Es ridículo vivir 100 años y sólo ser capaces de recordar 30 millones de " +
                         "bytes. O sea, menos que un compact disc. La condición humana se hace más " +
                         "obsoleta cada minuto",
                 author = "Marvin Minsky"
             ),
-            QuoteModel(
+            QuoteModel( //8
                 quote = "R2D2, ¿te lo dijo la computadora central de la ciudad? ¡R2D2, sabes bien " +
                         "que no debes confiar en una computadora extraña!",
                 author = "C3PO"
             ),
-            QuoteModel(
+            QuoteModel( //9
                 quote = "Nunca confíes en un ordenador que no puedas lanzar por una ventana",
                 author = "Steve Wozniak"
             ),
-            QuoteModel(
+            QuoteModel( //10
                 quote = "Hardware: las partes de un ordenador que pueden ser pateadas",
                 author = "Jeff Pesis"
             )
