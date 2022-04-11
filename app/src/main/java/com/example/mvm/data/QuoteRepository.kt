7521b3dd5886e5fd20d0aa0c76665a31efcd1744
Jorge Agulló <agullojorge@gmail.com>
2022-04-11 17:49:37 +0200

    Change response with hilt provide of quoteProvider

--- app/src/main/java/com/example/mvm/data/QuoteRepository.kt
+++ app/src/main/java/com/example/mvm/data/QuoteRepository.kt
@@ -6,12 +6,13 @@ import com.example.mvm.data.network.QuoteService
 import javax.inject.Inject
 
 class QuoteRepository @Inject constructor(
-    private val api:QuoteService
+    private val api:QuoteService,
+    private val quoteProvider: QuoteProvider
 ){
     suspend fun getAllQuotes():List<QuoteModel>{
         //Coroutine
         val response: List<QuoteModel> = api.getQuotes()
-        QuoteProvider.quotes = response
+        quoteProvider.quotes = response
         return response
     }
 }
