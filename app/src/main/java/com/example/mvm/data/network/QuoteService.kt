c74f8c74e080a5cf96310ed21cb78e5d9db5d0d7
Jorge Agulló <agullojorge@gmail.com>
2022-04-11 17:44:55 +0200

    Changed response wiht hilt provide of QuoteApiClient

--- app/src/main/java/com/example/mvm/data/network/QuoteService.kt
+++ app/src/main/java/com/example/mvm/data/network/QuoteService.kt
@@ -9,17 +9,14 @@ import javax.inject.Inject
 
 /*With this class we will connect to the repository which will decide where to get the data from,
 so if we change the backend service, we will only need to change this service.*/
-class QuoteService @Inject constructor(){
-
-    private val retrofit = RetrofitHelper.getRetrofit()
+class QuoteService @Inject constructor(
+    private val api:QuoteApiClient
+){
 
     suspend fun getQuotes():List<QuoteModel>{
         //Secondary thread
         return withContext(Dispatchers.IO) {
-            //We ask you to return a list of all quotes.
-            val response: Response<List<QuoteModel>> = retrofit.
-            create(QuoteApiClient::class.java).getAllQuotes()
-            //If we retrieve a null list or object, it will return an empty list.
+            val response = api.getAllQuotes()
             response.body() ?: emptyList()
         }
     }
