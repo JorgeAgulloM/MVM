0510daf170bd491ea7e59d26cdcec98b10d3061f
Jorge Agulló <agullojorge@gmail.com>
2022-04-11 16:39:33 +0200

    Prepared QuoteRepository for "hilt"

--- app/src/main/java/com/example/mvm/data/QuoteRepository.kt
+++ app/src/main/java/com/example/mvm/data/QuoteRepository.kt
@@ -3,8 +3,9 @@ package com.example.mvm.data
 import com.example.mvm.data.model.QuoteModel
 import com.example.mvm.data.model.QuoteProvider
 import com.example.mvm.data.network.QuoteService
+import javax.inject.Inject
 
-class QuoteRepository {
+class QuoteRepository @Inject constructor(){
 
     private val api = QuoteService()
 
