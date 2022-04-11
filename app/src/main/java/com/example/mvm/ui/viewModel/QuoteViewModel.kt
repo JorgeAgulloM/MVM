246f672f3a26052d222c0e2a4e2502b313555353
Jorge Agulló <agullojorge@gmail.com>
2022-04-11 15:26:24 +0200

    BugFix Update implements

--- app/src/main/java/com/example/mvm/ui/viewModel/QuoteViewModel.kt
+++ app/src/main/java/com/example/mvm/ui/viewModel/QuoteViewModel.kt
@@ -2,23 +2,44 @@ package com.example.mvm.ViewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.mvm.data.model.QuoteModel
 import com.example.mvm.data.model.QuoteProvider
+import com.example.mvm.domain.GetQuotesUseCase
+import com.example.mvm.domain.GetRandomQuoteUseCase
+import kotlinx.coroutines.launch
 
 //Extends of ViewModel
 class QuoteViewModel : ViewModel() {
 
     //Encapsulated the LiveData
     val quoteModel = MutableLiveData<QuoteModel>()
+    val isLoading = MutableLiveData<Boolean>()
+
+    var getQuotesUseCase = GetQuotesUseCase()
+    var getRandomQuoteUseCase = GetRandomQuoteUseCase()
+
+    fun onCreate() {
+        viewModelScope.launch {
+            isLoading.postValue(true)
+            val result = getQuotesUseCase()
+
+            if (!result.isNullOrEmpty()) {
+                quoteModel.postValue(result[0])
+                isLoading.postValue(false)
+            }
+        }
+    }
+
 
     //function to retrieve data once the user taps on the screen.
     fun randomQuote(){
         //A new input is requested to the model
-        val currentQuote : QuoteModel = QuoteProvider.random()
-        //and save in liveData
-        quoteModel.postValue(currentQuote)
-
-        /*LiveData will alert the activity that there is a change in the data and the activity
-        will do what it should do.*/
+        isLoading.postValue(true)
+        val quote = getRandomQuoteUseCase()
+        if (quote != null) {
+            quoteModel.postValue(quote!!)
+        }
+        isLoading.postValue(false)
     }
 }
