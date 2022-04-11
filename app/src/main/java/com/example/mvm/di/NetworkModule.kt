bb6774bf427fafbb0c8c4fe7b4bc0167f9286f4a
Jorge Agulló <agullojorge@gmail.com>
2022-04-11 17:39:03 +0200

    Crated module whit hilt and added retrofit provides

---
+++ app/src/main/java/com/example/mvm/di/NetworkModule.kt
@@ -0,0 +1,25 @@
+package com.example.mvm.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+//Provide library dependencies or dependencies on classes containing interfaces.
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit():Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://drawsomething-59328-default-rtdb.europe-west1.firebasedatabase.app/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+}
