5c9aab4eaefe2b5233877b5be03b54669360d50a
Jorge Agulló <agullojorge@gmail.com>
2022-04-11 17:52:32 +0200

    Changed to Singelton

--- app/src/main/java/com/example/mvm/data/model/QuoteProvider.kt
+++ app/src/main/java/com/example/mvm/data/model/QuoteProvider.kt
@@ -1,8 +1,10 @@
 package com.example.mvm.data.model
 
 import javax.inject.Inject
+import javax.inject.Singleton
 
 //Provider of data (example)
+@Singleton
 class QuoteProvider @Inject constructor(){
     var quotes:List<QuoteModel> = emptyList()
 }
