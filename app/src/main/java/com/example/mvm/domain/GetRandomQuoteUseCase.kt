9de771eb026b0ab695da6a8bae63158928176e8e
Jorge Agulló <agullojorge@gmail.com>
2022-04-11 17:53:29 +0200

    Added injection with hilt of quoteProvider

--- app/src/main/java/com/example/mvm/domain/GetRandomQuoteUseCase.kt
+++ app/src/main/java/com/example/mvm/domain/GetRandomQuoteUseCase.kt
@@ -4,10 +4,12 @@ import com.example.mvm.data.model.QuoteModel
 import com.example.mvm.data.model.QuoteProvider
 import javax.inject.Inject
 
-class GetRandomQuoteUseCase @Inject constructor(){
+class GetRandomQuoteUseCase @Inject constructor(
+    private val quoteProvider: QuoteProvider
+){
 
     operator fun invoke():QuoteModel?{
-        val quotes = QuoteProvider.quotes
+        val quotes = quoteProvider.quotes
         if(!quotes.isNullOrEmpty()){
             val randomNumber = (quotes.indices).random()
             return quotes[randomNumber]
