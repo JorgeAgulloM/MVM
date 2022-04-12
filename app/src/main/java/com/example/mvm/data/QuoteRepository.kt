5522999a2a3b72ec7a985e397e2e1d55dcf5b895
Jorge Agulló <agullojorge@gmail.com>
2022-04-12 14:02:27 +0200

    Change fun for get Quotes to from api & added fun get from DataBase

--- app/src/main/java/com/example/mvm/data/QuoteRepository.kt
+++ app/src/main/java/com/example/mvm/data/QuoteRepository.kt
@@ -1,18 +1,27 @@
 package com.example.mvm.data
 
+import com.example.mvm.data.dao.QuoteDao
+import com.example.mvm.data.database.entities.QuoteEntity
 import com.example.mvm.data.model.QuoteModel
 import com.example.mvm.data.model.QuoteProvider
 import com.example.mvm.data.network.QuoteService
+import com.example.mvm.domain.model.Quote
+import com.example.mvm.domain.model.toDomain
 import javax.inject.Inject
 
 class QuoteRepository @Inject constructor(
     private val api:QuoteService,
-    private val quoteProvider: QuoteProvider
+    private val quoteDao: QuoteDao
 ){
-    suspend fun getAllQuotes():List<QuoteModel>{
+    suspend fun getAllQuotesFromApi():List<Quote>{
         //Coroutine
-        val response = api.getQuotes()
-        quoteProvider.quotes = response
-        return response
+        val response: List<QuoteModel> = api.getQuotes()
+        //Mapper
+        return response.map { it.toDomain() }
+    }
+
+    suspend fun getAllQuotesFromDataBase():List<Quote>{
+        val response: List<QuoteEntity> = quoteDao.getAllQuotes()
+        return response.map { it.toDomain() }
     }
 }
