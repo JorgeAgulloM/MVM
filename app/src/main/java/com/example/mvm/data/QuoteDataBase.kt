d8646bc32dfe109316aeb176d36aa431637e7004
Jorge Agulló <agullojorge@gmail.com>
2022-04-12 14:36:58 +0200

    BugFix to use wrong class

--- app/src/main/java/com/example/mvm/data/QuoteDataBase.kt
+++ app/src/main/java/com/example/mvm/data/QuoteDataBase.kt
@@ -3,8 +3,9 @@ package com.example.mvm.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.mvm.data.dao.QuoteDao
+import com.example.mvm.data.database.entities.QuoteEntity
 
-@Database(entities = [QuoteDataBase::class], version = 1)
+@Database(entities = [QuoteEntity::class], version = 1)
 abstract class QuoteDataBase:RoomDatabase() {
 
     abstract fun getQuoteDao():QuoteDao
