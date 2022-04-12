3ce83b9b61ea3ef51c6e2aaa1b7689299f34f44f
Jorge Agulló <agullojorge@gmail.com>
2022-04-12 14:00:39 +0200

    Addeds Extension functions

--- app/src/main/java/com/example/mvm/domain/model/Quote.kt
+++ app/src/main/java/com/example/mvm/domain/model/Quote.kt
@@ -1,8 +1,12 @@
 package com.example.mvm.domain.model
 
-import androidx.room.ColumnInfo
+import com.example.mvm.data.database.entities.QuoteEntity
+import com.example.mvm.data.model.QuoteModel
 
 data class Quote(
     val quote:String,
     val author:String
     )
+
+fun QuoteModel.toDomain() = Quote(quote, author)
+fun QuoteEntity.toDomain() = Quote(quote, author)
