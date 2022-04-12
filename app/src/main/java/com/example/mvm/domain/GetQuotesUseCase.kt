e8ae3cbf27465855cb7bcf9fb91e1220d9140a2e
Jorge Agulló <agullojorge@gmail.com>
2022-04-12 14:15:27 +0200

    Added mapper & developed edit get quotes useCase

--- app/src/main/java/com/example/mvm/domain/GetQuotesUseCase.kt
+++ app/src/main/java/com/example/mvm/domain/GetQuotesUseCase.kt
@@ -1,14 +1,24 @@
 package com.example.mvm.domain
 
 import com.example.mvm.data.QuoteRepository
+import com.example.mvm.data.database.entities.toDataBase
 import com.example.mvm.data.model.QuoteModel
+import com.example.mvm.domain.model.Quote
 import javax.inject.Inject
 
 //Intercator
 class GetQuotesUseCase @Inject constructor(
     private val repository:QuoteRepository
 ) {
+    suspend operator fun invoke():List<Quote> {
+        val quotes = repository.getAllQuotesFromApi()
 
-    suspend operator fun invoke() = repository.getAllQuotes()
-
+        return if (quotes.isNotEmpty()) {
+            repository.clearQuotes()
+            repository.insertQuotes(quotes.map { it.toDataBase() })
+            quotes
+        } else {
+            repository.getAllQuotesFromDataBase()
+        }
+    }
 }
